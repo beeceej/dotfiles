1b80e43de9947a09a7af84eb5827ce2b7bd8e099
Brian Jones <beeceej.code@gmail.com>
2019-09-23 09:58:59 -0500

    prettier

--- .emacs.d/custom/lang/vue.el
+++ .emacs.d/custom/lang/vue.el
@@ -3,4 +3,9 @@
     :ensure t
     :config
     ;; 0, 1, or 2, representing (respectively) none, low, and high coloring
-    (setq mmm-submode-decoration-level 0)))
+    (setq mmm-submode-decoration-level 0))
+  (use-package prettier-js
+    :ensure t
+    :config
+    (add-hook 'vue-mode-hook 'prettier-js-mode))
+  )
